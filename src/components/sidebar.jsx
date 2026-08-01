e49053f4f3ee992312aadc827314a7e005cb3991
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-08-01 18:46:00 +0530

    sign-in-bar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,7 +5,17 @@ function Sidebar({open ,setopen}){
         <>
        
         <div className={` bg-white fixed right-0 top-0 h-screen w-[35%] transform transition-transform duration-500 ${open?" translate-x-0":"translate-x-full"}`}>
-             <X onClick={()=>setopen(false)}/>sidebar
+             <X onClick={()=>setopen(false)}/>
+               
+                <div className="container max-w-[80%] mx-auto mt-10 flex justify-between">
+                <div>
+                <span className="text-3xl font-bold">Login</span>
+                <div className="mt-3 text-orange-600 font-semibold">or create an account</div>
+                </div>
+                <div><img className=" h-30 w-30 object-cover"src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/Image-login_btpq7r" alt="" /></div>
+                </div>
+                <div className="container max-w-[90%] mx-auto text-center  ">  <input  className="py-5 px-12 border my-12 w-89"type="number" placeholder="Enter your phone number"></input>
+               <div><button className="bg-[#ff5200] py-5 px-10 w-89 text-white border ">LOG IN</button></div></div>
         </div>
         
         </>
